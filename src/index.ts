420d017225771322ed78276a88fe92e8cbecc72f
Jonny Wildey <jonnywildey@gmail.com>
2019-03-28 03:06:14 +0000

    correct type

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
+import { writeFileSync } from "fs";
 import * as ts from "typescript";
-
-export interface IGenerateFixtureParams {
-  name: string;
-  node: ts.Node;
-}
 
 function getTypeProperties(node: ts.Node) {
   const propertyArray: ts.PropertyAssignment[] = [];
   ts.forEachChild(node, cn => {
     if (cn.kind === ts.SyntaxKind.TypeLiteral) {
-      propertyArray.push(...getInterfaceProperties(cn));
+      return propertyArray.push(...getInterfaceProperties(cn));
     }
   });
   return propertyArray;
@@ -19,7 +15,7 @@ function getArrayLiterals(node: ts.Node) {
   const literalArray: ts.Expression[] = [];
   ts.forEachChild(node, cn => {
     if (cn.kind === ts.SyntaxKind.ArrayType) {
-      literalArray.push(...getArrayInterfaceProperties(cn));
+      return literalArray.push(...getArrayInterfaceProperties(cn));
     }
   });
   return literalArray;
@@ -37,7 +33,6 @@ function getArrayInterfaceProperties(node: ts.Node) {
 }
 
 function getArrayLiteral(node: ts.Node) {
-  console.log("kind", node);
   const type = node.kind;
   switch (type) {
     case ts.SyntaxKind.NumberKeyword:
@@ -66,13 +61,11 @@ function getInterfaceProperties(node: ts.Node) {
 }
 
 function getInterfacePropertyAssignment(node: ts.Node) {
-  console.log("kind", node);
   if (node.kind !== ts.SyntaxKind.PropertySignature) {
     return undefined;
   }
   const name = (node as any).name;
   const type = (node as any).type.kind;
-  console.log(type);
   switch (type) {
     case ts.SyntaxKind.NumberKeyword:
       return ts.createPropertyAssignment(name, ts.createNumericLiteral("5"));
@@ -94,8 +87,9 @@ function getInterfacePropertyAssignment(node: ts.Node) {
   }
 }
 
-function generateFixture({ name, node }: IGenerateFixtureParams) {
-  const fixtureName = ts.createIdentifier(`fixture${name}`);
+function generateFixture(node: ts.Node) {
+  const name = (node as any).name;
+  const fixtureName = ts.createIdentifier(`fixture${name.text}`);
 
   if (node.kind !== ts.SyntaxKind.InterfaceDeclaration) {
     throw new Error("Not interface");
@@ -105,7 +99,7 @@ function generateFixture({ name, node }: IGenerateFixtureParams) {
 
   const variable = ts.createVariableDeclaration(
     fixtureName,
-    ts.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+    (node as any).name,
     object
   );
   return ts.createVariableDeclarationList([variable], ts.NodeFlags.Const);
@@ -133,11 +127,10 @@ const printer = ts.createPrinter({
 });
 const result = printer.printNode(
   ts.EmitHint.Unspecified,
-  generateFixture({
-    name: "ITest",
-    node: sourceFile.statements[0]
-  }),
+  generateFixture(sourceFile.statements[0]),
   sourceFile
 );
+
+writeFileSync(`${__dirname}/../build/test.ts`, result);
 
 console.log(result);
